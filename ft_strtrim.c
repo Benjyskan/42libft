c7d8f2875b58d56312909c188ac3b60b1cd0682c
Piriou ENZO <penzo@e2r12p22.42.fr>
2018-11-18 17:51:49 +0100

    on en a gros2

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: penzo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/13 16:16:53 by penzo             #+#    #+#             */
-/*   Updated: 2018/11/18 17:43:20 by penzo            ###   ########.fr       */
+/*   Updated: 2018/11/18 17:51:27 by penzo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,9 @@ char	*ft_strtrim(char const *s)
 		i++;
 	res = &res[i];
 	res = ft_strrev(res);
-	if (!(res2 = ft_strnew(ft_strlen(res))))
+	return (res);
+	/*if (!(res2 = ft_strnew(ft_strlen(res))))
 		return (NULL);
 	ft_strcpy(res2, res);
-	return (res2);
+	return (res2);*/
 }
